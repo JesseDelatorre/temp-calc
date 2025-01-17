fca11739768c5cb9847e82bfafba0e22782026fe
Jesse <Existance@jesses-MacBook-Pro.local>
2025-01-17 17:34:02 -0500

    added plan for JS

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(`HELLO`)
+// create function called convertToCelsius which takes input and returns in celcius
+// make prompt for input F-temp
+//convert C-temp using equation
+//set temp description to equate F-temp
+//make temp description the same as weather description
+//create rules for <32
+//rules for <64
+// rules for <86
+//rules for <100
+//rules for >= 100
+//create alert that shows final message
+
+
